400766ae97e12f7a1110db5a1790ddb3db563e84
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-11 14:36:15 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name= (person_name)
+    @name=person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job= (jobname)
+    @job=breedname
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
